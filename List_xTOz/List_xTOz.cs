80899af9a1c571eaa1a8aa7eb397d3d896441e51
SeoDongHyeon0227 <83999403+SeoDongHyeon0227@users.noreply.github.com>
2021-05-18 22:49:38 +0900

    Create List_xTOz.cs

---
+++ List_xTOz/List_xTOz.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace x_n
+{
+    class Program
+    {
+        public static List<int> solution(int x, int n)
+        {
+            List<int> list = new List<int>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                list.Add(x * i);
+            }
+
+            foreach (int i in list)
+                Console.Write(i + " ");
+
+            return list;
+
+        }
+        static void Main(string[] args)
+        {
+            solution(2, 5);
+        }
+    }
+}
+
