75ae56bb8841fec3dcd3354b40233bc3b0a06b0a
SeoDongHyeon0227 <83999403+SeoDongHyeon0227@users.noreply.github.com>
2021-05-18 22:57:19 +0900

    Update List_xTOz.cs

--- List_xTOz/List_xTOz.cs
+++ List_xTOz/List_xTOz.cs
@@ -5,7 +5,7 @@ namespace x_n
 {
     class Program
     {
-        public static List<int> solution(int x, int n)
+        public static List<int> solution(int x, int n)  //바보같이 리턴 타입에 List<int>를 안 넣고 있어서 계속 헤맸다. 다음엔 이런 실수하지 않도록 하기.
         {
             List<int> list = new List<int>();
 
