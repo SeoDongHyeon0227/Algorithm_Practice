8084547f7ecc703378ade261dcc75703bd1b91e8
SeoDongHyeon0227 <83999403+SeoDongHyeon0227@users.noreply.github.com>
2021-05-25 15:03:12 +0900

    Create Dog.cs

---
+++ BaekJoon/Dog/Dog.cs
@@ -0,0 +1,17 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    Console.WriteLine ("|\\_/|");
+    Console.WriteLine ("|q p|   /}");
+    Console.WriteLine ("( 0 )\"\"\"\\");
+    Console.WriteLine ("|\"^\"`    |");
+    Console.WriteLine ("||_/=\\\\__|");
+  }
+}
+
+// |\_/|
+// |q p|   /}
+// ( 0 )"""\
+// |"^"`    |
+// ||_/=\\__|
